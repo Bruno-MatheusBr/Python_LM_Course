529e775ff2c19f0a33993dec90c5e08e0c197705
Bruno Matheus <bruno.mat@live.com>
2023-06-27 11:22:12 -0300

    Reviewed class 14

--- aula014.py
+++ aula014.py
@@ -1,5 +1,5 @@
-a = 'A'
-b = 'B'
+a = "A"
+b = "B"
 c = 1.1
 # string = 'a={} b={} c={:.2f} d={}'
 # formato = string.format(a, b, c, a)
@@ -10,6 +10,21 @@ c = 1.1
 # string = 'a={nome1} b={nome2} c={nome3}'
 # formato = string.format(nome1=a, nome2=b, nome3=c)
 
-formato = a, b
+string = "a={} b={} c={:.2f}".format(a, b, c)
+# formato = string.format(a, b, c)
+print(string)
 
-print(formato)
+# // Teste Pessoal
+nome = "Bruno"
+sobrenome = "Domingues"
+
+nome_completo = "{} {}".format(nome, sobrenome)
+print(nome_completo)
+
+# // PARÂMETRO NOMEADO
+print("")  # Espaçamento
+print("Formato Nomeado")  # Título
+
+string_2 = "b= {nome2} a= {nome1} c= {nome3:.2f}"
+formato_nomeado = string_2.format(nome1=a, nome2=b, nome3=c)
+print(formato_nomeado)
