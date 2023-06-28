f2810f2cf3d53e300a8f3bb97d183106bb73be4a
Bruno Matheus <bruno.mat@live.com>
2023-06-27 22:18:16 -0300

    Reviewed class 015

--- aula015.py
+++ aula015.py
@@ -1,10 +1,11 @@
 # nome = input('Qual o seu nome? ')
-#print(f'O seu nome é {nome}')
+# print(f'O seu nome é {nome}')
 
-numero_1 = input('digite um número: ')
-numero_2 = input('digite outro número: ')
+numero_1 = input("digite um número: ")
+numero_2 = input("digite outro número: ")
 
 int_numero_1 = int(numero_1)
 int_numero_2 = int(numero_2)
 
-print(f'A soma dos números é {int_numero_1 + int_numero_2}')
+print(f"A soma dos números é {int_numero_1 + int_numero_2}")
+print(f"A outra soma é {int_numero_1} + {int_numero_2}")  # Não vai somar.
