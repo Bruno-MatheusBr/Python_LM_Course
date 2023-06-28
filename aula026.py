0351d770a78a71591b0902ac09f471749475de53
Bruno Matheus <bruno.mat@live.com>
2023-06-28 13:45:44 -0300

    Class reviwed

--- aula026.py
+++ aula026.py
@@ -13,28 +13,33 @@ Sinal - + ou -
 Ex.: 0>-100,1.f
 Conversion flags - !r !s !a
 """
-variavel = 'ABC'
+variavel = "ABC"
 
-print(f'.{variavel:>8}.')
-print(f'.{variavel:0>8}.')
-print(f'.{variavel:<8}.')
-print(f'.{variavel:0<8}.')
-print(f'.{variavel:0^8}.')
+print(f".{variavel:>8}.")
+print(f".{variavel:0>8}.")  # Utilizado em contas numéricas
+print(f".{variavel:<8}.")
+print(f".{variavel:0<8}.")
+print(f".{variavel:0^8}.")
 print("")
-print('Restringindo casas decimais e colocando vírgula para separar milhar')
-print(f'{1000.848539457834}')
-print(f'{1000.848539457834:.2f}')
-print(f'{1000.848539457834:,.2f}')
-print('')
-print('Mostrando se o número é positivo (+) ou negativo (-)')
-print('Para mostrar os dois sinais, usa-se somente o sinal de + na F-String')
-print(f'{1000.848539457834:+,.2f}')
-print(f'{-1000.848539457834:+,.2f}')
-print('')
-print('Caso extremo: forçar o sistema a colocar zeros antes do número e o sinal de mais ou menos')
+print("Restringindo casas decimais e colocando vírgula para separar milhar")
+print(f"{1000.848539457834}")
+print(f"{1000.848539457834:.2f}")
+print(f"{1000.848539457834:,.2f}")
+print("")
+print("Mostrando se o número é positivo (+) ou negativo (-)")
+print("Para mostrar os dois sinais, usa-se somente o sinal de + na F-String")
+print(f"{1000.848539457834:+,.2f}")  # Importante
+print(f"{-1000.848539457834:+,.2f}")  # Importante
+print("")
+print(
+    "Caso extremo: forçar o sistema a colocar zeros antes do número e o sinal de mais ou menos"
+)
 print('Usa-se o sinal de "=" no lugar de ">"')
-print(f'{-1000.848539457834:0=+10,.2f}')
-print('')
-print('Hexadecimal com F-String')
-print(f'O Hexadeciaml de 1500 é {1500:x}')
-print(f'O Hexadeciaml de 1500 é {1500:05x}')  # Força o Hexadecimal ter 5 dígitos
+print(f"{-1000.848539457834:0=+10,.2f}")
+print("")
+print("Hexadecimal com F-String")
+print(f"O Hexadeciaml de 1500 é {1500:x}")
+print(f"O Hexadeciaml de 1500 é {1500:05x}")  # Força o Hexadecimal ter 5 dígitos
+numero = 2000
+
+print(f"O exadecimal de {numero} é {numero:05X}")
