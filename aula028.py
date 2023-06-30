f678e51df8c52923e98acd581f9dfb807f4d1fd4
Bruno Matheus <bruno.mat@live.com>
2023-06-30 18:02:40 -0300

    Class reviewed

--- aula028.py
+++ aula028.py
@@ -14,27 +14,26 @@ Se nada for digitado em nome ou idade:
     Exiba "Desculpe, você deixou campos vazios."
 
 """
-nome = input('Digite seu nome: ')
-idade = input('Digite sua idade: ')
-space = ' '
+# nome = input('Digite seu nome: ')
+# idade = input('Digite sua idade: ')
+# space = ' '
 
-if nome and idade:
-    print(f'Seu nome é {nome} e sua idade é {idade}.')
-    print(f'Seu nome invertido é {nome[::-1]}')
-    if space in nome:
-        print('Seu nome contém espaço.')
-    else:
-        print('Seu nome não contém espaço.')
-    print(f'Seu nome tem {len(nome)} letras.')
+# if nome and idade:
+#     print(f'Seu nome é {nome} e sua idade é {idade}.')
+#     print(f'Seu nome invertido é {nome[::-1]}')
+#     if space in nome:
+#         print('Seu nome contém espaço.')
+#     else:
+#         print('Seu nome não contém espaço.')
+#     print(f'Seu nome tem {len(nome)} letras.')
 
-    print(f'A primeira letra do seu nome é {nome[0]}.')
+#     print(f'A primeira letra do seu nome é {nome[0]}.')
 
-    # ultima_letra = nome[len(nome)+1]
-    print(f'A última letra do seu nome é {nome[len(nome)-1]}.')
+#     # ultima_letra = nome[len(nome)+1]
+#     print(f'A última letra do seu nome é {nome[len(nome)-1]}.')
 
-else:
-    print('Desculpe, vc deixou campos vazios.')
-
+# else:
+#     print('Desculpe, vc deixou campos vazios.')
 
 
 # name = input('Digite seu nome: ')
@@ -58,10 +57,27 @@ else:
 #         print(f'Seu nome contém {n}')
 #     else:
 #         print(f'Seu nome não contém {n}')
-        
+
 #     print(f'A primeira letra do seu nome é {name[0]}')
 #  #   print(f'A última letra do seu nome é {name[len(name)-1]}')
 #     print(f'A última letra do seu nome é {name[-1]}')
 
 # else:
 #     print("Desculpe, você deixou campos vazios")
+
+nome = input("Digite seu nome: ")
+idade = input("Digite sua idade: ")
+
+if nome and idade:
+    print(f'Seu nome é "{nome}".')
+    print(f"Sua idade é {idade}.")
+    print(f"Seu nome invertido é {nome[::-1]}.")
+    if " " in nome:
+        print(f"Seu nome contém espaço.")
+    else:
+        print("Seu nome não contém espaço.")
+    print(f"Seu nome tem {len(nome)} letras.")
+    print(f'A primeira letra do seu nome é "{nome[0]}".')
+    print(f'A última letra do seu nome é "{nome[-1]}".')
+else:
+    print("Desculpe, você deixou campos vazios.")
