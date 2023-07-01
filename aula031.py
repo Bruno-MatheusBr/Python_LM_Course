fc64a2461afdd62a503422b7cfaa1e33e30a1abf
Bruno Matheus <bruno.mat@live.com>
2023-07-01 11:10:11 -0300

    Class Reviewed

--- aula031.py
+++ aula031.py
@@ -13,15 +13,15 @@ passou_no_if = None
 
 if condicao:
     passou_no_if = True
-    print('Faça algo')
+    print("Faça algo")
 else:
-    print('Não faça algo')
+    print("Não faça algo")
 
 print(passou_no_if, passou_no_if is None)
 print(passou_no_if, passou_no_if is not None)
 
 if passou_no_if is None:
-    print('Não passou no if')
+    print("Não passou no if")
 
 if passou_no_if is not None:
-    print('Passou no if')
+    print("Passou no if")
