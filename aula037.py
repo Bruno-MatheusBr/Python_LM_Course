ed6ff1899b75cb7c59a1044a242e16b321dff08a
Bruno Matheus <bruno.mat@live.com>
2023-07-04 18:16:48 -0300

    Class Reviewed

--- aula037.py
+++ aula037.py
@@ -1,12 +1,25 @@
+# contador = 0
+
+# while contador <= 100:
+#     contador += 1
+
+#     if contador > 10 and contador < 30:
+#         print('Não vou mostrar o ', contador)
+#         continue
+#     print(contador)
+
+#     if contador == 40:
+#         break
+
 contador = 0
 
-while contador <= 100:
+while contador < 100:
     contador += 1
-
-    if contador > 10 and contador < 30:
-        print('Não vou mostrar o ', contador)
+    if contador > 10 and contador < 20:
         continue
     print(contador)
 
     if contador == 40:
         break
+
+print("Acabou!!! 🙌")
--- aula038.py
+++ aula038.py
@@ -1,18 +1,32 @@
 # while dentro de while
 
-qtd_linhas = 5
-qtd_colunas = 5
+# qtd_linhas = 5
+# qtd_colunas = 5
 
-linha = 1
+# linha = 1
 
-while linha <= qtd_linhas:
-    coluna = 1
+# while linha <= qtd_linhas:
+#     coluna = 1
 
-    while coluna <= qtd_colunas:
-        print(f'{linha=} {coluna=}')
-        coluna += 1
-    
+#     while coluna <= qtd_colunas:
+#         print(f'{linha=} {coluna=}')
+#         coluna += 1
+
+#     linha += 1
+
+# print('Acabou!')
+
+nr_linhas = 5
+nr_colunas = 5
+
+linha = 0
+coluna = 0
+
+while linha < nr_linhas:
     linha += 1
+    while coluna < nr_colunas:
+        coluna += 1
+        print(f"Linha {linha} - Coluna {coluna}")
+    coluna = 0
 
-print('Acabou!')
-
+print("Acabou 🙌🙌🙌")
